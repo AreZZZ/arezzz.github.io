3b9ce8eb4f0f3a9a4b627c2a57d504d3cda5006c
AreZZZ <36213408+AreZZZ@users.noreply.github.com>
2019-04-28 20:53:13 +0200

    A
    A

--- data.js
+++ data.js
@@ -1,46 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-poziom2_1",
-      "name": "Poziom2_1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": -0.046989633707534395,
-        "pitch": 0.13049654085633478,
-        "fov": 1.4134061960355204
-      },
-      "linkHotspots": [
-        {
-          "yaw": -0.32605607789085944,
-          "pitch": 0.1937981166191065,
-          "rotation": 0,
-          "target": "1-poziom2_2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-poziom2_2",
-      "name": "Poziom2_2",
+      "id": "0-sala-jzyk-polski",
+      "name": "Sala Język Polski",
       "levels": [
         {
           "tileSize": 256,
@@ -68,154 +30,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.4432568688183052,
-          "pitch": 0.07300339622068286,
+          "yaw": 0.8998851091573741,
+          "pitch": 0.14484695385666413,
           "rotation": 0,
-          "target": "0-poziom2_1"
-        },
-        {
-          "yaw": -1.4754284907293496,
-          "pitch": 0.06378943627121636,
-          "rotation": 0,
-          "target": "2-poziom2_3"
+          "target": "5-pitro-1-rodkowa-cz"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-poziom2_3",
-      "name": "Poziom2_3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
-        "fov": 1.4134061960355204
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.4918448537152518,
-          "pitch": 0.17838372654929735,
-          "rotation": 0,
-          "target": "1-poziom2_2"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "3-poziom1_1",
-      "name": "Poziom1_1",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0,
-        "pitch": 0,
-        "fov": 1.4134061960355204
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-poziom1_2",
-      "name": "Poziom1_2",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 3.079770943741414,
-        "pitch": 0.04210643827886784,
-        "fov": 1.4134061960355204
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "5-poziom_1_3",
-      "name": "Poziom_1_3",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 3.0077575740878064,
-        "pitch": 0.10300522338629925,
-        "fov": 1.4134061960355204
-      },
-      "linkHotspots": [],
-      "infoHotspots": []
-    },
-    {
-      "id": "6-sala-polski",
-      "name": "Sala Polski",
+      "id": "1-sala-przyrodnicza",
+      "name": "Sala Przyrodnicza",
       "levels": [
         {
           "tileSize": 256,
@@ -241,11 +66,62 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 0.653725727147684,
+          "pitch": 0.07565332259070345,
+          "rotation": 0,
+          "target": "8-pitro-2-rodkowa-cz"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "7-szatnia",
+      "id": "2-sala-gimnastyczna",
+      "name": "Sala Gimnastyczna",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1920,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6122210788129347,
+          "pitch": 0.07754839705322425,
+          "rotation": 0,
+          "target": "3-szatnia"
+        },
+        {
+          "yaw": 2.732757426204447,
+          "pitch": 0.07753713508648197,
+          "rotation": 0,
+          "target": "4-pitro-1-prawa-cz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-szatnia",
       "name": "Szatnia",
       "levels": [
         {
@@ -268,16 +144,29 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": 0,
         "pitch": 0,
-        "fov": 1.4134061960355204
+        "yaw": 0,
+        "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -1.4157958477550618,
+          "pitch": -0.015966979906831824,
+          "rotation": 0,
+          "target": "4-pitro-1-prawa-cz"
+        },
+        {
+          "yaw": -1.7133845058199686,
+          "pitch": -0.009895503893263324,
+          "rotation": 0,
+          "target": "3-szatnia"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "8-poziom3_1",
-      "name": "Poziom3_1",
+      "id": "4-pitro-1-prawa-cz",
+      "name": "Piętro 1 Prawa Część",
       "levels": [
         {
           "tileSize": 256,
@@ -299,16 +188,341 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -3.0198302243645507,
-        "pitch": 0.14609097906864577,
-        "fov": 1.4134061960355204
+        "yaw": -0.37513062641771455,
+        "pitch": 0.11456428167557675,
+        "fov": 1.4095469020700806
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": -0.47730062535408635,
+          "pitch": 0.10066041900853406,
+          "rotation": 0,
+          "target": "5-pitro-1-rodkowa-cz"
+        },
+        {
+          "yaw": 1.5914529911086994,
+          "pitch": 0.012059535218170936,
+          "rotation": 0,
+          "target": "7-pitro-2-prawa-cz"
+        },
+        {
+          "yaw": 1.296239533190434,
+          "pitch": 0.5433293752842943,
+          "rotation": 0,
+          "target": "3-szatnia"
+        },
+        {
+          "yaw": 1.1295608409491624,
+          "pitch": 0.528003646374394,
+          "rotation": 0,
+          "target": "2-sala-gimnastyczna"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "9-poziom3_3",
-      "name": "Poziom3_3",
+      "id": "5-pitro-1-rodkowa-cz",
+      "name": "Piętro 1 Środkowa Część",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 3.116473111027446,
+        "pitch": 0.06683066536818139,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8814616214138766,
+          "pitch": 0.039185270667898564,
+          "rotation": 0,
+          "target": "4-pitro-1-prawa-cz"
+        },
+        {
+          "yaw": 1.9807706864645995,
+          "pitch": 0.07994901955398959,
+          "rotation": 6.283185307179586,
+          "target": "0-sala-jzyk-polski"
+        },
+        {
+          "yaw": 1.8550817355819111,
+          "pitch": 0.08284615734880774,
+          "rotation": 4.71238898038469,
+          "target": "6-pitro-1-lewa-cz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-pitro-1-lewa-cz",
+      "name": "Piętro 1 Lewa Część",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -3.0936813050509233,
+        "pitch": 0.021842734845009204,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.514061995775373,
+          "pitch": 0.12063301619444822,
+          "rotation": 0,
+          "target": "5-pitro-1-rodkowa-cz"
+        },
+        {
+          "yaw": 1.1719581552979363,
+          "pitch": -0.004294546218794437,
+          "rotation": 0,
+          "target": "9-pitro-2-lewa-cz"
+        },
+        {
+          "yaw": 1.4814773178331562,
+          "pitch": 0.4769475208018896,
+          "rotation": 0,
+          "target": "3-szatnia"
+        },
+        {
+          "yaw": 1.628546496524847,
+          "pitch": 0.48042644084270236,
+          "rotation": 0,
+          "target": "2-sala-gimnastyczna"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-pitro-2-prawa-cz",
+      "name": "Piętro 2 Prawa Część",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1854743509859187,
+        "pitch": 0.17469799540564068,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3702787075270955,
+          "pitch": 0.44029973513196197,
+          "rotation": 0,
+          "target": "4-pitro-1-prawa-cz"
+        },
+        {
+          "yaw": 1.7404078450321565,
+          "pitch": -0.0280298155601848,
+          "rotation": 0,
+          "target": "10-pitro-3-prawa-cz"
+        },
+        {
+          "yaw": -0.49528168087741875,
+          "pitch": 0.24464716630648198,
+          "rotation": 0,
+          "target": "8-pitro-2-rodkowa-cz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-pitro-2-rodkowa-cz",
+      "name": "Piętro 2 Środkowa Część",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.0137361541563816,
+        "pitch": 0.031070873477910155,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.2427430138396947,
+          "pitch": 0.11226055538702795,
+          "rotation": 0,
+          "target": "1-sala-przyrodnicza"
+        },
+        {
+          "yaw": 1.451024858171257,
+          "pitch": 0.07612814094802545,
+          "rotation": 0,
+          "target": "7-pitro-2-prawa-cz"
+        },
+        {
+          "yaw": -1.455102943687395,
+          "pitch": 0.08058797123592676,
+          "rotation": 0,
+          "target": "9-pitro-2-lewa-cz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-pitro-2-lewa-cz",
+      "name": "Piętro 2 Lewa Część",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.11610586420963998,
+        "pitch": 0.11955362113040557,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.5207728424828897,
+          "pitch": 0.12944488205793547,
+          "rotation": 0,
+          "target": "8-pitro-2-rodkowa-cz"
+        },
+        {
+          "yaw": -1.177053879177592,
+          "pitch": 0.4382363842553936,
+          "rotation": 0,
+          "target": "6-pitro-1-lewa-cz"
+        },
+        {
+          "yaw": -1.748322787304108,
+          "pitch": 0.06251088242382608,
+          "rotation": 0,
+          "target": "12-pitro-3-lewa-cz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-pitro-3-prawa-cz",
+      "name": "Piętro 3 Prawa Część",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.948395314442184,
+        "pitch": 0.12496040668717079,
+        "fov": 1.4095469020700806
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.598649185610725,
+          "pitch": 0.550906004683938,
+          "rotation": 0,
+          "target": "10-pitro-3-prawa-cz"
+        },
+        {
+          "yaw": 2.4524029886746597,
+          "pitch": 0.22379086611903176,
+          "rotation": 0,
+          "target": "11-pitro-3-rodkowa-cz"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-pitro-3-rodkowa-cz",
+      "name": "Piętro 3 Środkowa Część",
       "levels": [
         {
           "tileSize": 256,
@@ -334,12 +548,25 @@ var APP_DATA = {
         "yaw": 0,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.3688865145138127,
+          "pitch": 0.06405907947206302,
+          "rotation": 0,
+          "target": "10-pitro-3-prawa-cz"
+        },
+        {
+          "yaw": -1.308737036433003,
+          "pitch": 0.0822149136304482,
+          "rotation": 12.566370614359176,
+          "target": "12-pitro-3-lewa-cz"
+        }
+      ],
       "infoHotspots": []
     },
     {
-      "id": "10-pano_20190311_151157",
-      "name": "PANO_20190311_151157",
+      "id": "12-pitro-3-lewa-cz",
+      "name": "Piętro 3 Lewa Część",
       "levels": [
         {
           "tileSize": 256,
@@ -361,19 +588,32 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
+        "yaw": 1.0781676807318465,
+        "pitch": 0.1565687077262261,
+        "fov": 1.4095469020700806
       },
-      "linkHotspots": [],
+      "linkHotspots": [
+        {
+          "yaw": 1.4711872162275155,
+          "pitch": 0.5074601152588407,
+          "rotation": 6.283185307179586,
+          "target": "6-pitro-1-lewa-cz"
+        },
+        {
+          "yaw": -2.2349981683837594,
+          "pitch": 0.21193460471681824,
+          "rotation": 6.283185307179586,
+          "target": "11-pitro-3-rodkowa-cz"
+        }
+      ],
       "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Szkoła Podstawowa Nr 8 im. Dar Górników 360",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": true,
-    "viewControlButtons": false
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": true
   }
 };
